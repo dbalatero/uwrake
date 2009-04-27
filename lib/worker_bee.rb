9908f57bad1aa7a5577ed40def80977a19d8827a
David Balatero <dbalatero@evri.com>
2009-04-26 17:06:12 -0700

    * Added gitignores for vim files. * Added specs for worker bee. * Still need to pass specs for WorkerBee#run.

--- lib/worker_bee.rb
+++ lib/worker_bee.rb
@@ -1,3 +1,31 @@
+require 'ostruct'
+
 class WorkerBee
   VERSION = '1.0.0'
+
+  @@work = {}
+
+  def self.recipe(&block)
+    raise ArgumentError, "WorkerBee#recipe requires a block." unless block_given?
+    instance_eval(&block)
+  end
+
+  def self.reset_work
+    @@work = {}
+  end
+
+  def self.run(name)
+    
+  end
+
+  def self.work(name, *deps, &work)
+    raise ArgumentError, "WorkerBee#work requires a block." unless block_given?
+
+    @@work[name] = OpenStruct.new(:work => work,
+                                  :dependencies => deps)
+  end
+
+  def self.work_count
+    @@work.size
+  end
 end
---
+++ spec/worker_bee_spec.rb
@@ -0,0 +1,53 @@
+require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
+
+describe WorkerBee do
+  describe "recipe" do
+    it "should require a block of code to be passed in" do
+      lambda { WorkerBee.recipe }.should raise_error(ArgumentError)
+    end
+
+    it "should execute the passed block in the correct context" do
+      block = lambda { 5 + 5 }
+      WorkerBee.should_receive(:instance_eval).and_yield
+      WorkerBee.recipe(&block)
+    end
+  end
+
+  describe "run" do
+    it "should execute the code for given task name" do
+      run_count = 0
+      WorkerBee.work(:foo) { run_count += 1 }
+
+      WorkerBee.run(:foo)
+      run_count.should == 1
+    end
+
+    it "should execute dependencies in order" do
+      execution_path = []
+      WorkerBee.work(:first) { execution_path << :first }
+      WorkerBee.work(:second, :first) { execution_path << :second }
+
+      WorkerBee.run(:second)
+      execution_path.should == [:first, :second]
+    end
+  end
+
+  describe "work" do
+    before(:each) do
+      # Have a clean state each test.
+      WorkerBee.reset_work
+    end
+
+    it "should require a recipe block to be passed in" do
+      lambda { WorkerBee.work(:name) }.should raise_error(ArgumentError)
+    end
+
+    it "should save a given block under a recipe name" do
+      lambda {
+        WorkerBee.work(:foo) do
+          puts "hello!"
+        end
+      }.should change(WorkerBee, :work_count).by(1)
+    end
+  end
+end
